12385755342e0b731021931148fa070263726d3c
tmgree <tmgree@outlook.com>
2016-03-03 20:05:02 +0000

    Fully completed using  Sieve of Eratosthenes

--- Ruby/10001_prime/10001_prime.rb
+++ Ruby/10001_prime/10001_prime.rb
@@ -1,17 +1,17 @@
 def primes
    prime_array =[];
    number=1;
-   until prime_array.length == 10000 do
+   until prime_array.length == 10001 do
      prime_array_add(number, is_prime(number), prime_array)
      number += 2
    end
-   prime_array.last
+   prime_array[10000]
 end
 
 
 def is_prime(n)
   divisor = [];
-  for i in 2...n do
+  for i in 2...(Math.sqrt(n)+1) do
     divisor.push(n % i)
   end
   !does_include_zero(divisor)
