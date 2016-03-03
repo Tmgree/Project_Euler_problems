d945dd32b054b5b5a2bd3960a0a2c5d422149d06
tmgree <tmgree@outlook.com>
2016-03-03 17:22:02 +0000

    even fib done for javascript

--- javascript/Even_fib/src/even_fib.js
+++ javascript/Even_fib/src/even_fib.js
@@ -1,3 +1,28 @@
+var fib_array = [1, 2];
+var fib_even_sum = 2;
+var index = 2;
+var fib_array_last = fib_array[fib_array.length - 1];
+
 function even_fib() {
-    return 4613732
+    fib_last = fib_array[index-1]+fib_array[index-2];
+    while (is_larger(fib_last)!==false){
+    index +=1;
+    fib_last = fib_array[index-1]+fib_array[index-2];
+    sum_even(fib_last);
+  }
+    return(fib_even_sum);
+}
+
+function is_larger(n) {
+  if (n < 4000000) {
+    fib_array.push(n);
+    return(true);
+  }
+  else {return(false);}
+}
+
+function sum_even(n) {
+  if (n%2 ===0) {
+    fib_even_sum += n;
+  }
 }
