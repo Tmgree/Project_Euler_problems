81ad98968c8815e638ca46d85c3ca8c635aee571
tmgree <tmgree@outlook.com>
2016-03-15 14:19:20 +0000

    rspec updated

--- Ruby/Largest_prime_factor/prime_factor.rb
+++ Ruby/Largest_prime_factor/prime_factor.rb
@@ -1,9 +1,30 @@
-def prime_factor(n)
-  for i in 2..(n/2.to_i) do
+require 'prime'
 
-  end
+def prime_factor(n)
+  factors_array = [];
+  is_divisible(n, factors_array)
+  # factors_array.delete_if {|n| is_prime(n) == false }
+  factors_array.max
 end
 
-def is_divisible(n,x)
+def is_divisible(n, array)
+  prime_array = Prime.first(n/2.to_i)
+   for i in (0..(n/4.to_i)) do
+       if n % prime_array[i] ==0
+         array.push(prime_array[i])
+       end
+   end
+   array
+end
 
+def is_prime(n)
+  divisor = [];
+  for i in 2...(Math.sqrt(n)+1) do
+    divisor.push(n % i)
+  end
+  !does_include_zero(divisor)
+end
+
+def does_include_zero(array)
+  array.include?(0)
 end
---
+++ Ruby/Pythagorean_Triplet/spec/Pythagorean_triplet_spec.rb
@@ -0,0 +1,10 @@
+require './Pythagorean_triplet'
+
+
+describe '#find_c' do
+
+it "it should return the correct value of the hypotenuse given two values " do
+  expect(find_c(3,4)).to eq(5)
+end
+
+end
